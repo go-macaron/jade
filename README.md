ce74bab20d36444ec5bc097d542b7d7ad1993a0a
codeskyblue <codeskyblue@gmail.com>
2015-09-29 14:03:17 +0800

    change for macaron

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Martini-jade
 Martini middleware/handler for easily rendering serialized JSON and HTML template responses from Jade templates.
 
-[API Reference](http://godoc.org/github.com/codegangsta/martini-contrib/render)
+[API Reference](http://godoc.org/github.com/codeskyblue/macaron-jade)
 
 ## Usage
 This middleware uses Jade implementation in Go [go-floki/jade](https://github.com/go-floki/jade) to render Jade templates.
@@ -11,16 +11,16 @@ This middleware uses Jade implementation in Go [go-floki/jade](https://github.co
 package main
 
 import (
-  "github.com/codegangsta/martini"
-  "github.com/frogprog/martini-jade"
+  "github.com/Unknwon/macaron"
+  "github.com/codeskyblue/macaron-jade"
 )
 
 func main() {
-  m := martini.Classic()
+  m := macaron.Classic()
   // render html templates from templates directory
-  m.Use(render.Renderer())
+  m.Use(jade.Renderer())
 
-  m.Get("/", func(r render.Render) {
+  m.Get("/", func(r jade.Render) {
     r.HTML(200, "hello", map[string]string {
        "foo": "bar",
     })
@@ -37,11 +37,11 @@ func main() {
 ~~~
 
 ### Options
-`render.Renderer` comes with a variety of configuration options:
+`jade.Renderer` comes with a variety of configuration options:
 
 ~~~ go
 // ...
-m.Use(render.Renderer(render.Options{
+m.Use(jade.Renderer(jade.Options{
   Directory: "templates", // Specify what path to load the templates from.
   Layout: "layout", // Specify a layout template. Layouts can call {{ yield }} to render the current template.
   Extensions: []string{".jade"}, // Specify extensions to load for templates.
@@ -53,7 +53,7 @@ m.Use(render.Renderer(render.Options{
 ~~~
 
 ### Loading Templates
-By default the `render.Renderer` middleware will attempt to load templates with a '.jade' extension from the "templates" directory. Templates are found by traversing the templates directory and are named by path and basename. For instance, the following directory structure:
+By default the `jade.Renderer` middleware will attempt to load templates with a '.jade' extension from the "templates" directory. Templates are found by traversing the templates directory and are named by path and basename. For instance, the following directory structure:
 
 ~~~
 templates/
@@ -75,27 +75,27 @@ home
 ~~~
 
 ### Character Encodings
-The `render.Renderer` middleware will automatically set the proper Content-Type header based on which function you call. See below for an example of what the default settings would output (note that UTF-8 is the default):
+The `jade.Renderer` middleware will automatically set the proper Content-Type header based on which function you call. See below for an example of what the default settings would output (note that UTF-8 is the default):
 ~~~ go
 // main.go
 package main
 
 import (
-  "github.com/codegangsta/martini"
-  "github.com/frogprog/martini-jade"
+  "github.com/Unknwon/macaron"
+  "github.com/codeskyblue/macaron-jade"
 )
 
 func main() {
-  m := martini.Classic()
-  m.Use(render.Renderer())
+  m := macaron.Classic()
+  m.Use(jade.Renderer())
 
   // This will set the Content-Type header to "text/html; charset=UTF-8"
-  m.Get("/", func(r render.Render) {
+  m.Get("/", func(r jade.Render) {
     r.HTML(200, "hello", "world")
   })
 
   // This will set the Content-Type header to "application/json; charset=UTF-8"
-  m.Get("/api", func(r render.Render) {
+  m.Get("/api", func(r jade.Render) {
     r.JSON(200, map[string]interface{}{"hello": "world"})
   })
 
@@ -104,29 +104,29 @@ func main() {
 
 ~~~
 
-In order to change the charset, you can set the `Charset` within the `render.Options` to your encoding value:
+In order to change the charset, you can set the `Charset` within the `jade.Options` to your encoding value:
 ~~~ go
 // main.go
 package main
 
 import (
-  "github.com/codegangsta/martini"
-  "github.com/frogprog/martini-jade"
+  "github.com/Unknwon/macaron"
+  "github.com/codeskyblue/macaron-jade"
 )
 
 func main() {
-  m := martini.Classic()
-  m.Use(render.Renderer(render.Options{
+  m := macaron.Classic()
+  m.Use(jade.Renderer(render.Options{
     Charset: "ISO-8859-1",
   }))
 
   // This is set the Content-Type to "text/html; charset=ISO-8859-1"
-  m.Get("/", func(r render.Render) {
+  m.Get("/", func(r jade.Render) {
     r.HTML(200, "hello", "world")
   })
 
   // This is set the Content-Type to "application/json; charset=ISO-8859-1"
-  m.Get("/api", func(r render.Render) {
+  m.Get("/api", func(r jade.Render) {
     r.JSON(200, map[string]interface{}{"hello": "world"})
   })
 
@@ -139,3 +139,5 @@ func main() {
 * [Jeremy Saenz](http://github.com/codegangsta)
 * [Cory Jacobsen](http://github.com/cojac)
 * [frogprog](http://github.com/frogprog)
+* [codeskyblue](http://github.com/codeskyblue)
+* [Unknwon](http://github.com/Unknwon)
--- render_test.go
+++ render_test.go
@@ -1,111 +1,113 @@
-package render
+package jade
 
 import (
-    "github.com/codegangsta/martini"
-    "net/http"
-    "net/http/httptest"
-    "reflect"
-    "testing"
-    "net/url"
-    "html/template"
+	"html/template"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"github.com/Unknwon/macaron"
+	//"github.com/codegangsta/macaron"
 )
 
 type Greeting struct {
-    One string `json:"one"`
-    Two string `json:"two"`
+	One string `json:"one"`
+	Two string `json:"two"`
 }
 
 func Test_Render_JSON(t *testing.T) {
-    m := martini.Classic()
-    m.Use(Renderer(Options{
-        // nothing here to configure
-    }))
+	m := macaron.Classic()
+	m.Use(Renderer(Options{
+	// nothing here to configure
+	}))
 
-    // routing
-    m.Get("/foobar", func(r Render) {
-        r.JSON(300, Greeting{"hello", "world"})
-    })
+	// routing
+	m.Get("/foobar", func(r Render) {
+		r.JSON(300, Greeting{"hello", "world"})
+	})
 
-    res := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/foobar", nil)
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/foobar", nil)
 
-    m.ServeHTTP(res, req)
+	m.ServeHTTP(res, req)
 
-    expect(t, res.Code, 300)
-    expect(t, res.Header().Get(ContentType), ContentJSON+"; charset=UTF-8")
-    expect(t, res.Body.String(), `{"one":"hello","two":"world"}`)
+	expect(t, res.Code, 300)
+	expect(t, res.Header().Get(ContentType), ContentJSON+"; charset=UTF-8")
+	expect(t, res.Body.String(), `{"one":"hello","two":"world"}`)
 }
 
 func Test_Render_Indented_JSON(t *testing.T) {
-    m := martini.Classic()
-    m.Use(Renderer(Options{
-        IndentJSON: true,
-    }))
+	m := macaron.Classic()
+	m.Use(Renderer(Options{
+		IndentJSON: true,
+	}))
 
-    // routing
-    m.Get("/foobar", func(r Render) {
-        r.JSON(300, Greeting{"hello", "world"})
-    })
+	// routing
+	m.Get("/foobar", func(r Render) {
+		r.JSON(300, Greeting{"hello", "world"})
+	})
 
-    res := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/foobar", nil)
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/foobar", nil)
 
-    m.ServeHTTP(res, req)
+	m.ServeHTTP(res, req)
 
-    expect(t, res.Code, 300)
-    expect(t, res.Header().Get(ContentType), ContentJSON+"; charset=UTF-8")
-    expect(t, res.Body.String(), `{
+	expect(t, res.Code, 300)
+	expect(t, res.Header().Get(ContentType), ContentJSON+"; charset=UTF-8")
+	expect(t, res.Body.String(), `{
   "one": "hello",
   "two": "world"
 }`)
 }
 
 func Test_Render_Bad_HTML(t *testing.T) {
-    m := martini.Classic()
-    m.Use(Renderer(Options{
-        Directory: "fixtures/basic",
-    }))
+	m := macaron.Classic()
+	m.Use(Renderer(Options{
+		Directory: "fixtures/basic",
+	}))
 
-    // routing
-    m.Get("/foobar", func(r Render) {
-        r.HTML(200, "nope", nil)
-    })
+	// routing
+	m.Get("/foobar", func(r Render) {
+		r.HTML(200, "nope", nil)
+	})
 
-    res := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/foobar", nil)
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/foobar", nil)
 
-    m.ServeHTTP(res, req)
+	m.ServeHTTP(res, req)
 
-    expect(t, res.Code, 500)
-    expect(t, res.Body.String(), "html/template: \"nope\" is undefined\n")
+	expect(t, res.Code, 500)
+	expect(t, res.Body.String(), "html/template: \"nope\" is undefined\n")
 }
 
 func Test_Render_HTML(t *testing.T) {
-    m := martini.Classic()
-    m.Use(Renderer(Options{
-        Directory: "fixtures/basic",
-    }))
+	m := macaron.Classic()
+	m.Use(Renderer(Options{
+		Directory: "fixtures/basic",
+	}))
 
-    // routing
-    m.Get("/foobar", func(r Render) {
-        r.HTML(200, "hello", map[string]string {
-            "foo": "jeremy",
-        })
-    })
+	// routing
+	m.Get("/foobar", func(r Render) {
+		r.HTML(200, "hello", map[string]string{
+			"foo": "jeremy",
+		})
+	})
 
-    res := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/foobar", nil)
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/foobar", nil)
 
-    m.ServeHTTP(res, req)
+	m.ServeHTTP(res, req)
 
-    expect(t, res.Code, 200)
-    expect(t, res.Header().Get(ContentType), ContentHTML+"; charset=UTF-8")
-    expect(t, res.Body.String(), "<h1>Hello jeremy</h1>\n")
+	expect(t, res.Code, 200)
+	expect(t, res.Header().Get(ContentType), ContentHTML+"; charset=UTF-8")
+	expect(t, res.Body.String(), "<h1>Hello jeremy</h1>\n")
 }
 
 /*
 func Test_Render_Extensions(t *testing.T) {
-    m := martini.Classic()
+    m := macaron.Classic()
     m.Use(Renderer(Options{
         Directory:  "fixtures/basic",
         Extensions: []string{".tmpl", ".html"},
@@ -128,105 +130,105 @@ func Test_Render_Extensions(t *testing.T) {
 */
 func Test_Render_Funcs(t *testing.T) {
 
-    m := martini.Classic()
-    m.Use(Renderer(Options{
-        Directory: "fixtures/basic",
-        Funcs: []template.FuncMap{
-            {
-                "foo": func() string {
-                    return "My custom function"
-                },
-            },
-        },
-    }))
+	m := macaron.Classic()
+	m.Use(Renderer(Options{
+		Directory: "fixtures/basic",
+		Funcs: []template.FuncMap{
+			{
+				"foo": func() string {
+					return "My custom function"
+				},
+			},
+		},
+	}))
 
-    // routing
-    m.Get("/foobar", func(r Render) {
-        r.HTML(200, "custom-func", "jeremy")
-    })
+	// routing
+	m.Get("/foobar", func(r Render) {
+		r.HTML(200, "custom-func", "jeremy")
+	})
 
-    res := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/foobar", nil)
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/foobar", nil)
 
-    m.ServeHTTP(res, req)
+	m.ServeHTTP(res, req)
 
-    expect(t, res.Body.String(), "My custom function\n")
+	expect(t, res.Body.String(), "My custom function\n")
 }
 
 func Test_Render_Error404(t *testing.T) {
-    res := httptest.NewRecorder()
-    r := renderer{res, nil, nil, Options{}, ""}
-    r.Error(404)
-    expect(t, res.Code, 404)
+	res := httptest.NewRecorder()
+	r := renderer{res, nil, nil, Options{}, ""}
+	r.Error(404)
+	expect(t, res.Code, 404)
 }
 
 func Test_Render_Error500(t *testing.T) {
-    res := httptest.NewRecorder()
-    r := renderer{res, nil, nil, Options{}, ""}
-    r.Error(500)
-    expect(t, res.Code, 500)
+	res := httptest.NewRecorder()
+	r := renderer{res, nil, nil, Options{}, ""}
+	r.Error(500)
+	expect(t, res.Code, 500)
 }
 
 func Test_Render_Redirect_Default(t *testing.T) {
-    url, _ := url.Parse("http://localhost/path/one")
-    req := http.Request{
-        Method: "GET",
-        URL:    url,
-    }
-    res := httptest.NewRecorder()
+	url, _ := url.Parse("http://localhost/path/one")
+	req := http.Request{
+		Method: "GET",
+		URL:    url,
+	}
+	res := httptest.NewRecorder()
 
-    r := renderer{res, &req, nil, Options{}, ""}
-    r.Redirect("two")
+	r := renderer{res, &req, nil, Options{}, ""}
+	r.Redirect("two")
 
-    expect(t, res.Code, 302)
-    expect(t, res.HeaderMap["Location"][0], "/path/two")
+	expect(t, res.Code, 302)
+	expect(t, res.HeaderMap["Location"][0], "/path/two")
 }
 
 func Test_Render_Redirect_Code(t *testing.T) {
-    url, _ := url.Parse("http://localhost/path/one")
-    req := http.Request{
-        Method: "GET",
-        URL:    url,
-    }
-    res := httptest.NewRecorder()
+	url, _ := url.Parse("http://localhost/path/one")
+	req := http.Request{
+		Method: "GET",
+		URL:    url,
+	}
+	res := httptest.NewRecorder()
 
-    r := renderer{res, &req, nil, Options{}, ""}
-    r.Redirect("two", 307)
+	r := renderer{res, &req, nil, Options{}, ""}
+	r.Redirect("two", 307)
 
-    expect(t, res.Code, 307)
-    expect(t, res.HeaderMap["Location"][0], "/path/two")
+	expect(t, res.Code, 307)
+	expect(t, res.HeaderMap["Location"][0], "/path/two")
 }
 
 func Test_Render_Charset_JSON(t *testing.T) {
-    m := martini.Classic()
-    m.Use(Renderer(Options{
-        Charset: "foobar",
-    }))
+	m := macaron.Classic()
+	m.Use(Renderer(Options{
+		Charset: "foobar",
+	}))
 
-    // routing
-    m.Get("/foobar", func(r Render) {
-        r.JSON(300, Greeting{"hello", "world"})
-    })
+	// routing
+	m.Get("/foobar", func(r Render) {
+		r.JSON(300, Greeting{"hello", "world"})
+	})
 
-    res := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/foobar", nil)
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/foobar", nil)
 
-    m.ServeHTTP(res, req)
+	m.ServeHTTP(res, req)
 
-    expect(t, res.Code, 300)
-    expect(t, res.Header().Get(ContentType), ContentJSON+"; charset=foobar")
-    expect(t, res.Body.String(), `{"one":"hello","two":"world"}`)
+	expect(t, res.Code, 300)
+	expect(t, res.Header().Get(ContentType), ContentJSON+"; charset=foobar")
+	expect(t, res.Body.String(), `{"one":"hello","two":"world"}`)
 }
 
 /* Test Helpers */
 func expect(t *testing.T, a interface{}, b interface{}) {
-    if a != b {
-        t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
-    }
+	if a != b {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
 }
 
 func refute(t *testing.T, a interface{}, b interface{}) {
-    if a == b {
-        t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
-    }
+	if a == b {
+		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
 }
